d2967b1ff59d9fbc307ea788a2da93cb501327d8
HanSeo <khan3618@naver.com>
2020-12-19 22:22:50 +0900

    Create customize.lua

---
+++ cfg/customize.lua
@@ -0,0 +1,11 @@
+local cfg = {}
+
+cfg.font = {
+    ["Arita-dotum-Medium"] = "아리따 돋움",
+    ["NanumBarunGothic"] = "나눔 바른 고딕",
+    ["yg-jalnan"] = "잘난체",
+    ["YiSunShinDotumM"] = "이순신돋움체",
+    ["Daehan-Bold"] = "대한체"
+}
+
+return cfg
